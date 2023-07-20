17875d0ad327a9c233b397c1ca83b8c15723f35d
nikita <paramonov20@bk.ru>
2023-07-20 13:56:49 +0300

    Узнал, про 5 праивило использование корутин Structured Concurrency, Exception Handling

--- app/src/main/java/com/example/startcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/startcoroutines/MainViewModel.kt
@@ -2,18 +2,24 @@ package com.example.startcoroutines
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.lang.RuntimeException
 import kotlin.concurrent.thread
 
 class MainViewModel : ViewModel() {
 
     private val parentJob = Job()
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.d(LOG_TAG, "Exception caught: $throwable")
+    }
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob + exceptionHandler)
     private val childJob = Job()
 
     fun method() {
@@ -26,19 +32,22 @@ class MainViewModel : ViewModel() {
             }
             val subChildJob2 = coroutineScope.launch(childJob) {
                 delay(3000)
+                error()
                 Log.d(LOG_TAG, "second subChild coroutine finished")
             }
-            Log.d(LOG_TAG, childJob.children.contains(subChildJob1).toString())
-            Log.d(LOG_TAG, childJob.children.contains(subChildJob2).toString())
         }
-
         val childJob2 = coroutineScope.launch {
             delay(2000)
             Log.d(LOG_TAG, "second coroutine finished")
-            Log.d(LOG_TAG, "Parent job canceled: ${parentJob.isCancelled}")
         }
-        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString())
-        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString())
+        val childJob3 = coroutineScope.launch {
+            delay(1000)
+            Log.d(LOG_TAG, "third coroutine finished")
+        }
+    }
+
+    private fun error() {
+        throw RuntimeException()
     }
 
     override fun onCleared() {
